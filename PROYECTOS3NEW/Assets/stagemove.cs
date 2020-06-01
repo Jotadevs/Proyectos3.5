d62e240652f5f9a689c217f2b476013dbba2fdfc
Sergio_gonzalez <iruecha9000@yahoo.es>
2020-06-02 01:55:55 +0200

    Mapa blocking bro
    Pues el mapa está marcado

---
+++ PROYECTOS3NEW/Assets/stagemove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stagemove : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.x -= speed * Time.deltaTime;
+        transform.position = temp;
+    }
+}
