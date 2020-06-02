fd816d1b5b69049937b1b41f42d180f2cc60e06e
Jose Javier Megias Valero <josejaviermegias@gmail.com>
2020-06-02 21:59:13 +0200

    Canvas con lo de las vidas

---
+++ PROYECTOS3NEW/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public Vidas vidaCanvas;
+    public int vida = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaCanvas = GameObject.FindObjectOfType<Vidas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            vida -= 1;
+            vidaCanvas.CambioVida(vida);
+        }
+        
+    }
+}
