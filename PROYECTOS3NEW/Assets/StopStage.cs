81875618f3ef3eb9324a1b76a3b8b497dbc413a1
Jose Javier Megias Valero <josejaviermegias@gmail.com>
2020-06-05 00:29:35 +0200

    No se Cosas y el boss

---
+++ PROYECTOS3NEW/Assets/StopStage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopStage : MonoBehaviour
+{
+    public stagemove stage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = GameObject.FindObjectOfType<stagemove>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Stage")
+        {
+            Debug.Log("Entra");
+            stage.speed = 0;
+        }
+    }
+}
